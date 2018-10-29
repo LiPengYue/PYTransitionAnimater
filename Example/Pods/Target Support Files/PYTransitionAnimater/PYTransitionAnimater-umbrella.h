54bc99627963ec50c550c2a0b5f0f7e3ee220354
李鹏跃 <702029772@qq.com>
2018-10-29 10:33:08 +0800

    [pod install]

--- Example/Pods/Target Support Files/PYTransitionAnimater/PYTransitionAnimater-umbrella.h
+++ Example/Pods/Target Support Files/PYTransitionAnimater/PYTransitionAnimater-umbrella.h
@@ -12,6 +12,8 @@
 
 #import "AnimatedTransition.h"
 #import "Animater.h"
+#import "BasePresentViewController.h"
+#import "BasePresentViewControllerConfiguration.h"
 
 FOUNDATION_EXPORT double PYTransitionAnimaterVersionNumber;
 FOUNDATION_EXPORT const unsigned char PYTransitionAnimaterVersionString[];
